1fb4cb8dd9dca6dd296f0261e898695eedc4500a
Robert Nagy <ronag89@gmail.com>
2017-03-19 20:31:49 +0100

    fix(listener): Ignore unsoliticted messages.

--- src/utils/listener.js
+++ src/utils/listener.js
@@ -122,8 +122,6 @@ Listener.prototype._$onMessage = function (message) {
     this._callback(message.data[1], true, this._createCallbackResponse(message))
   } else if (message.action === C.ACTIONS.SUBSCRIPTION_FOR_PATTERN_REMOVED) {
     this._callback(message.data[1], false)
-  } else {
-    this._client._$onError(this._topic, C.EVENT.UNSOLICITED_MESSAGE, `${message.data[0]}|${message.data[1]}`)
   }
 }
 
