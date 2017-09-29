78b7efadc6ba57cd3c309c742c4bb7321acbf7db
Robert Nagy <ronagy@icloud.com>
2017-09-29 15:53:20 +0200

    fix(config): default maxReconnectAttemps should be infinite

--- src/default-options.js
+++ src/default-options.js
@@ -3,8 +3,8 @@
 module.exports = {
   heartbeatInterval: 30000,
   reconnectIntervalIncrement: 4000,
-  maxReconnectInterval: 180000,
-  maxReconnectAttempts: 5,
+  maxReconnectInterval: 120000,
+  maxReconnectAttempts: Infinity,
   maxMessagesPerPacket: 128,
   sendDelay: 10,
   maxIdleTime: 500,
