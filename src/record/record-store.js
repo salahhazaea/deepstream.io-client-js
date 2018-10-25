c0048a377d38b4420cba0ffa008f7aad1be710e3
Robert Nagy <ronagy@icloud.com>
2018-10-25 11:04:18 +0200

    fix(record-store): bulk ops

--- src/record/record-store.js
+++ src/record/record-store.js
@@ -6,62 +6,95 @@ function RecordStore (options, handler) {
   this._db = options.db
   this._handler = handler
 
-  this._docs = []
-  this._sync = null
+  this._write = []
+  this._writing = null
+
+  this._read = []
+  this._reading = null
 }
 
 RecordStore.prototype.get = function (name, callback) {
-  const val = this._lru.get(name)
-  if (val) {
-    callback(null, val[0], val[1])
+  const entry = this._lru.get(name)
+  if (entry) {
+    const [ data, version ] = entry
+    callback(null, data, version)
   } else if (this._db) {
-    // TODO (perf): allDocs
-    this._db.get(name, (err, doc) => {
-      if (err) {
-        callback(err)
-      } else {
-        const version = doc._rev
-        if (!doc._deleted) {
-          delete doc._id
-          delete doc._rev
-          callback(null, doc, version)
-        } else {
-          callback(null)
-        }
-      }
-    })
+    this._read.push({ name, callback })
+    this._flushRead()
   } else {
-    callback(null, null)
+    callback(null)
   }
 }
 
 RecordStore.prototype.set = function (name, data, version) {
   if (this._db && /^[^I0]/.test(version)) {
-    this._docs.push(Object.assign({ _id: name, _rev: version }, data))
-    this._flush()
+    this._write.push(Object.assign({ _id: name, _rev: version }, data))
+    this._flushWrite()
   }
 
   this._lru.set(name, [ data, version ])
 }
 
-RecordStore.prototype._flush = function () {
-  if (this._sync || this._docs.length === 0) {
+RecordStore.prototype._flushRead = function () {
+  if (this._reading || this._read.length === 0) {
     return
   }
 
-  const docs = this._docs.splice(0, 256)
-  this._sync = this._handler
-    .sync()
-    .then(() => utils.schedule(() => {
-      this._sync = null
-      this._db
-        .bulkDocs(docs, { new_edits: false }, err => {
-          if (err) {
-            console.error(err)
+  this._reading = setTimeout(() => {
+    const read = this._read.splice(0, 256)
+    this._readSync = this._db
+      .allDocs({
+        keys: read.map(({ name }) => name),
+        include_docs: true
+      })
+      .then(({ rows }) => {
+        for (let n = 0; n < read.length; ++n) {
+          const { doc } = rows[n]
+          const { callback } = read[n]
+
+          if (!doc._deleted) {
+            const version = doc._rev
+            delete doc._id
+            delete doc._rev
+            callback(null, doc, version)
+          } else {
+            callback(null)
           }
-          this._flush()
+        }
+      })
+      .catch(err => {
+        for (let n = 0; n < read.length; ++n) {
+          const callback = read[n][1]
+          callback(err)
+        }
+      })
+      .then(() => {
+        this._reading = false
+        this._flushRead()
+      })
+  }, 1)
+}
+
+RecordStore.prototype._flushWrite = function () {
+  if (this._writing || this._write.length === 0) {
+    return
+  }
+
+  this._writing = setTimeout(() => {
+    const docs = this._write.splice(0, 256)
+    this._handler
+      .sync()
+      .then(() => utils.schedule(() => this._db
+        .bulkDocs(docs, { new_edits: false })
+        .catch(err => {
+          console.error(err)
         })
-    }))
+        .then(() => {
+          this._writing = null
+          this._flushWrite()
+        })
+      ))
+  }, 1)
 }
 
 module.exports = RecordStore
