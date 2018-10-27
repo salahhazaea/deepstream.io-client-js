163fbc29404e92e135ac8394edd7649439a7373e
Robert Nagy <ronagy@icloud.com>
2018-10-27 15:33:28 +0200

    fix(listener): removed broken null path

--- src/utils/listener.js
+++ src/utils/listener.js
@@ -68,8 +68,6 @@ Listener.prototype._$onMessage = function (message) {
     provider.observer = {
       next: value => {
         if (value == null) {
-          provider.value$ = null
-          this._connection.sendMsg(this._topic, C.ACTIONS.LISTEN_REJECT, [ this._pattern, name ])
           return
         }
 
