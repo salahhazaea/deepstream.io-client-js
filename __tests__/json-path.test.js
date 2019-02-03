f743e3280e278f9c6173d9b54bd637088d0e7969
Robert Nagy <ronagy@icloud.com>
2019-02-03 18:16:23 +0100

    test: more json-path test

--- __tests__/json-path.test.js
+++ __tests__/json-path.test.js
@@ -1,42 +1,7 @@
 
 const jsonPath = require('../src/record/json-path')
 
-describe('undefined props', () => {
-  it ('ignore undefined updates', () => {
-    const val1 = {
-      type: 'event',
-      parent: 'MYZRXbx919v_Sv',
-      time: 1548968391.703,
-      duration: 796.8669998645782
-    }
-    expect(jsonPath.set(val1, undefined, {
-      ...val1,
-      gallery: undefined,
-      rundown: undefined
-    })).toBe(val1)
-  })
-
-  it ('ignore undefined updates on empty object', () => {
-    const val1 = {}
-    expect(jsonPath.set(val1, undefined, {
-      ...val1,
-      gallery: undefined,
-      rundown: undefined
-    })).toBe(val1)
-  })
-
-  it ('ignore undefined updates on new object', () => {
-    const val1 = {
-      time: 1
-    }
-    const res = jsonPath.set(val1, undefined, {
-      ...val1,
-      time: undefined
-    })
-    expect(res).not.toBe(val1)
-    expect(Object.keys(res)).toEqual([])
-  })
-
+describe('equality', () => {
   it ('deep equal', () => {
     const obj = { asd: 123 }
     const res = jsonPath.set({
@@ -45,57 +10,200 @@ describe('undefined props', () => {
     }, null, {
       obj
     })
+
     expect(res.obj).toBe(obj)
     expect(res).toEqual({ obj })
   })
-})
 
-describe('equality', () => {
-  it('return old value', () => {
-    function test(val1, val2) {
-      return jsonPath.set(val1, undefined, val2) == val1
+  it('returns old value', () => {
+    const obj = {
+      asd: true
     }
-
-    expect(test({
+    const res = jsonPath.set(obj, undefined, {
       asd: true
-    }, {
-      asd: true
-    })).toBe(true)
+    })
 
-    expect(test({
-      asd: true
-    }, {
-      asd: true,
-      foo: undefined
-    })).toBe(true)
-
-    expect(test({}, {})).toBe(true)
-    expect(test({}, { foo: undefined })).toBe(true)
+    expect(res).toBe(obj)
   })
 })
 
 describe('order', () => {
   it('updates order', () => {
-    const val1 = {
+    const obj = {
       foo: 1,
       bar: 1
     }
-    const res = jsonPath.set(val1, undefined, {
+    const res = jsonPath.set(obj, undefined, {
       bar: 1,
-      foo: 1
+      foo: 2
     })
-    expect(res).not.toBe(val1)
+
+    expect(res).not.toBe(obj)
+    expect(res).toEqual({ bar: 1, foo: 2})
   })
 
   it('keeps order', () => {
-    const val1 = {
+    const obj = {
       foo: 1,
       bar: 1
     }
-    const res = jsonPath.set(val1, undefined, {
+    const res = jsonPath.set(obj, undefined, {
       foo: 1,
       bar: 1
     })
-    expect(res).toBe(val1)
+
+    expect(res).toBe(obj)
+  })
+})
+
+
+describe('paths are tokenized and retrieved correctly', () => {
+  const testRecord = { _$data: {
+    firstname: 'Wolfram',
+    lastname: 'Hempel',
+    address:{
+      street: 'currentStreet'
+    },
+    pastAddresses: [
+      { street: 'firststreet', postCode: 1001 },
+      { street: 'secondstreet', postCode: 2002 }
+    ],
+    1234: 'integer index'
+  } }
+
+  it('retrieves simple paths', () => {
+    expect(jsonPath.get(testRecord._$data, 'firstname')).toBe('Wolfram')
+  })
+
+  it('retrieves nested paths', () => {
+    expect(jsonPath.get(testRecord._$data, 'address.street')).toBe('currentStreet')
+  })
+
+  it('retrieves array entries', () => {
+    expect(jsonPath.get(testRecord._$data, 'pastAddresses[1]')).toEqual({ street: 'secondstreet', postCode: 2002 })
+  })
+
+  it('retrieves other array entries', () => {
+    expect(jsonPath.get(testRecord._$data, 'pastAddresses[0]')).toEqual({ street: 'firststreet', postCode: 1001 })
+  })
+
+  it('retrieves values from objects within arrays', () => {
+    expect(jsonPath.get(testRecord._$data, 'pastAddresses[0].postCode')).toBe(1001)
+  })
+
+  it('handles whitespace', () => {
+    expect(jsonPath.get(testRecord._$data, ' pastAddresses[ 1 ].postCode ')).toBe(2002)
+  })
+
+  it('handles integers', () => {
+    expect(jsonPath.get(testRecord._$data, 1234)).toBe('integer index')
+  })
+
+  it('returns undefined for non existing keys', () => {
+    expect(jsonPath.get(testRecord._$data, 'doesNotExist')).toBe(undefined)
+  })
+
+  it('returns undefined for non existing nested keys', () => {
+    expect(jsonPath.get(testRecord._$data, 'address.number')).toBe(undefined)
+  })
+
+  it('returns undefined for existing array indices', () => {
+    expect(jsonPath.get(testRecord._$data, 'pastAddresses[3]')).toBe(undefined)
+  })
+
+  it('returns undefined for negative array indices', () => {
+    expect(jsonPath.get(testRecord._$data, 'pastAddresses[-1]')).toBe(undefined)
+  })
+
+  it('detects changes', () => {
+    expect(jsonPath.get(testRecord._$data, 'firstname')).toBe('Wolfram')
+    testRecord._$data.firstname = 'Egon'
+    expect(jsonPath.get(testRecord._$data, 'firstname')).toBe('Egon')
+  })
+
+  it('detects changes to arrays', () => {
+    expect(jsonPath.get(testRecord._$data, 'pastAddresses[1].street')).toBe('secondstreet')
+    testRecord._$data.pastAddresses.pop()
+    expect(jsonPath.get(testRecord._$data, 'pastAddresses[1].street')).toBe(undefined)
+  })
+})
+
+describe('objects are created from paths and their value is set correctly', () => {
+  it('sets simple values', () => {
+    const record = { _$data:{} }
+    record._$data = jsonPath.set(record._$data, 'firstname', 'Wolfram')
+    expect(jsonPath.get(record._$data, 'firstname')).toBe('Wolfram')
+    expect(record._$data).toEqual({ firstname: 'Wolfram' })
+  })
+
+  it('sets values for nested objects', () => {
+    const record = { _$data:{} }
+    record._$data = jsonPath.set(record._$data, 'adress.street', 'someStreet')
+    expect(jsonPath.get(record._$data, 'adress.street')).toBe('someStreet')
+    expect(record._$data).toEqual({
+      adress: {
+        street: 'someStreet'
+      }
+    })
+  })
+
+  it('sets values for arrays', () => {
+    const record = { _$data:{} }
+    record._$data = jsonPath.set(record._$data, 'pastAddresses[1].street', 'someStreet')
+    expect(jsonPath.get(record._$data, 'pastAddresses[1].street')).toBe('someStreet')
+    expect(record._$data).toEqual({
+      pastAddresses: [
+        undefined,
+        {
+          street: 'someStreet'
+        }]
+    })
+  })
+
+  it('sets values for null values', () => {
+    const record = { _$data:{
+      job: null,
+  			joinedAt: 1480020987915
+    } }
+    const jobId = { id: 88 }
+    record._$data = jsonPath.set(record._$data, 'job', jobId, true)
+    expect(jsonPath.get(record._$data, 'job.id')).toBe(88)
+    expect(record._$data).toEqual({
+      job: { id: 88 },
+      joinedAt: 1480020987915
+    })
+  })
+
+  it('extends existing objects', () => {
+    const record = { _$data: { firstname: 'Wolfram' } }
+    record._$data = jsonPath.set(record._$data, 'lastname', 'Hempel')
+    expect(jsonPath.get(record._$data, 'lastname')).toBe('Hempel')
+    expect(record._$data).toEqual({
+      firstname: 'Wolfram',
+      lastname: 'Hempel'
+    })
+  })
+
+  it('even when the path is not NaNish and could be interpreted as a base 16 number', () => {
+    let record = {}
+    const pathName = '0x02335'
+    record = jsonPath.set(record, pathName, 'value')
+    expect(record[0]).toBe(undefined)
+    expect(record[pathName]).toBe('value')
+    expect(jsonPath.get(record, pathName)).toBe('value')
+    expect(record[pathName]).toBe('value')
+  })
+
+  it('extends existing arrays', () => {
+    const record = { _$data: {
+      firstname: 'Wolfram',
+      animals: ['Bear', 'Cow', 'Ostrich']
+    } }
+    record._$data = jsonPath.set(record._$data, 'animals[ 1 ]', 'Emu')
+    expect(jsonPath.get(record._$data, 'animals[ 1 ]')).toBe('Emu')
+    expect(record._$data).toEqual({
+      firstname: 'Wolfram',
+      animals: ['Bear', 'Emu', 'Ostrich']
+    })
   })
 })
