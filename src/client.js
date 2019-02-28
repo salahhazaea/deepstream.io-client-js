f808b8fb7bab2f916716056eea39e92de7b66fe1
Robert Nagy <ronagy@icloud.com>
2019-02-28 10:29:49 +0100

    fix(client9: maintain username over logins

--- src/client.js
+++ src/client.js
@@ -30,8 +30,6 @@ const Client = function (url, options) {
 Emitter(Client.prototype)
 
 Client.prototype.login = function (authParamsOrCallback, callback) {
-  // TODO (fix): This is not correct if login is called multiple times.
-  this.username = null
   if (typeof authParamsOrCallback === 'function') {
     this._connection.authenticate({}, (success, authData) => {
       this.username = authData && authData.username
