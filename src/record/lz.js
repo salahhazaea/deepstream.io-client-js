1502fe62b0fd900f4030c2fa3c28b27082fd2073
Robert Nagy <ronagy@icloud.com>
2019-08-26 21:50:35 +0200

    Revert "fix(listener): make local update JSON compatible"
    This reverts commit b3dc2e8473c209f5a7fc01302a514b02bdbd0ca1.

--- src/record/lz.js
+++ src/record/lz.js
@@ -58,7 +58,6 @@ if (process && process.version && /v(\d[2-9]|[2-9]\d)/.test(process.version)) {
     })
   }
 } else {
-  // TODO: inline webworker here
   const lz = require('@nxtedition/lz-string')
 
   LZ.prototype.compress = function (obj, cb) {
