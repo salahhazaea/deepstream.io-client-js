4a7706873155f0c2d89a268b1c4522c22ccdb579
Robert Nagy <ronagy@icloud.com>
2022-11-14 14:46:20 +0100

    fix: replace READ with SUBSCRIBE

--- src/record/record.js
+++ src/record/record.js
@@ -468,9 +468,9 @@ Record.prototype._subscribe = function () {
   // TODO (fix): Limit number of reads.
 
   if (this._entry[0]) {
-    this._connection.sendMsg(C.TOPIC.RECORD, C.ACTIONS.READ, [this.name, this._entry[0]])
+    this._connection.sendMsg(C.TOPIC.RECORD, C.ACTIONS.SUBSCRIBE, [this.name, this._entry[0]])
   } else {
-    this._connection.sendMsg(C.TOPIC.RECORD, C.ACTIONS.READ, [this.name])
+    this._connection.sendMsg(C.TOPIC.RECORD, C.ACTIONS.SUBSCRIBE, [this.name])
   }
 
   if (this._updates) {
