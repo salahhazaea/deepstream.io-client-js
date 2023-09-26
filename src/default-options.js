fad8b3f62bd34ed3d3645525a022af5d2bb5dac6
Robert Nagy <ronagy@icloud.com>
2023-09-26 09:12:48 +0200

    fix: bump maxPacketSize

--- src/default-options.js
+++ src/default-options.js
@@ -2,7 +2,7 @@ module.exports = {
   reconnectIntervalIncrement: 1e3,
   maxReconnectInterval: 6e3,
   maxReconnectAttempts: Infinity,
-  maxPacketSize: 512 * 1024,
+  maxPacketSize: 1024 * 1024,
   batchSize: 4096,
   schedule: null,
   logger: null,
