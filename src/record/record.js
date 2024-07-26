5e969758544b4a91d61306d975b56bc09f3c6a97
Robert Nagy <ronagy@icloud.com>
2024-07-26 18:17:44 +0200

    fix: revert provider

--- src/record/record.js
+++ src/record/record.js
@@ -251,7 +251,7 @@ class Record {
           onDone(
             Object.assign(new Error(`timeout  ${this.name} [${current}<${expected}]`), {
               code: 'ETIMEDOUT',
-            })
+            }),
           )
         }, timeout)
       }
@@ -392,7 +392,7 @@ class Record {
     this._version = nextVersion
   }
 
-  _onUpdate([, version, data, hasProvider]) {
+  _onUpdate([, version, data]) {
     const prevData = this._data
     const prevVersion = this._version
     const prevState = this._state
@@ -425,11 +425,17 @@ class Record {
       this._onPatching(false)
     }
 
-    this._state = hasProvider != null && convertTyped(hasProvider, this._handler._client) === true
-      ? C.RECORD_STATE.PROVIDER
-      : this._version.charAt(0) === 'I'
-      ? C.RECORD_STATE.STALE
-      : C.RECORD_STATE.SERVER
+    if (this._state < C.RECORD_STATE.SERVER) {
+      this._state = this._version.charAt(0) === 'I' ? C.RECORD_STATE.STALE : C.RECORD_STATE.SERVER
+    }
+
+    // if (hasProvider != null ) {
+    //   this._state = convertTyped(hasProvider, this._handler._client)
+    //     ? C.RECORD_STATE.PROVIDER
+    //     : this._version.charAt(0) === 'I'
+    //     ? C.RECORD_STATE.STALE
+    //     : C.RECORD_STATE.SERVER
+    // }
 
     if (this._state !== prevState || this._data !== prevData || this._version !== prevVersion) {
       this._emitUpdate()
@@ -475,8 +481,8 @@ class Record {
       hasProvider && convertTyped(hasProvider, this._handler._client)
         ? C.RECORD_STATE.PROVIDER
         : this._version.charAt(0) === 'I'
-        ? C.RECORD_STATE.STALE
-        : C.RECORD_STATE.SERVER
+          ? C.RECORD_STATE.STALE
+          : C.RECORD_STATE.SERVER
 
     if (this._state !== prevState) {
       this._emitUpdate()
